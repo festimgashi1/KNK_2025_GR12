67784748a1e5fbe9cd95c4111a306a051a40f44e
GresaThaci <gresa.thaci@student.uni-pr.edu>
2025-04-05 23:29:42 +0200

    feedback started

--- src/main/java/model/Feedback.java
+++ src/main/java/model/Feedback.java
@@ -1,4 +1,10 @@
 package model;
 
 public class Feedback {
+    private int fb_id;
+    private int costumerId;
+    private int flightId;
+    private int rating;
+    private String comments;
+    private String status;
 }
