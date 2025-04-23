8c982384cc5604198b304ad3474ea17d152ffa44
GresaThaci <gresa.thaci@student.uni-pr.edu>
2025-04-23 22:36:19 +0200

    udpate createbookingdto

--- src/main/java/model/dto/CreateBookingDto.java
+++ src/main/java/model/dto/CreateBookingDto.java
@@ -1,4 +1,22 @@
 package model.dto;
 
+import java.time.LocalDateTime;
+
 public class CreateBookingDto {
+    private Long costumerId;
+
+    private Long flightsNumber;
+    private LocalDateTime dataENisjes;
+    private String destinacioni;
+    private Double cmimi;
+
+    public CreateBookingDto(Long costumerId, Long flightsNumber, LocalDateTime dataENisjes, String destinacioni, Double cmimi) {
+        this.costumerId = costumerId;
+        this.flightsNumber = flightsNumber;
+        this.dataENisjes = dataENisjes;
+        this.destinacioni = destinacioni;
+        this.cmimi = cmimi;
+    }
+
+
 }
