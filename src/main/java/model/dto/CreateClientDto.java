a699a06869d069d50279e7a2ab6c1a3900823392
Gresaaaa <ferataj.gresa@gmail.com>
2025-04-23 22:30:27 +0200

    update clientDto

--- src/main/java/model/dto/CreateClientDto.java
+++ src/main/java/model/dto/CreateClientDto.java
@@ -1,5 +1,33 @@
 package model.dto;
 
+import java.time.LocalDate;
+
 public class CreateClientDto {
-    
+private int clientId;
+private String firstName;
+private String lastName;
+private String phoneNumber;
+private String address;
+private String city;
+private String country;
+private String postalClient;
+private LocalDate registrationDate;
+private String passportNumber;
+private String nationality;
+
+
+    public CreateClientDto(int clientId, String firstName, String lastName, String phoneNumber, String address, String city, String country, String postalClient, LocalDate registrationDate, String passportNumber, String nationality) {
+        this.clientId = clientId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+        this.city = city;
+        this.country = country;
+        this.postalClient = postalClient;
+        this.registrationDate = registrationDate;
+        this.passportNumber = passportNumber;
+        this.nationality = nationality;
+    }
+
 }
