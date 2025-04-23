442157aab377df4831891b5b878a165f9ef2cbba
GresaThaci <gresa.thaci@student.uni-pr.edu>
2025-04-23 23:09:36 +0200

    update flightsstatusdto

--- src/main/java/model/dto/FlightsStatusDto.java
+++ src/main/java/model/dto/FlightsStatusDto.java
@@ -1,4 +1,21 @@
 package model.dto;
 
+import java.time.LocalDateTime;
+
 public class FlightsStatusDto {
+    private Long flightsId;
+    private String statusi;
+    private LocalDateTime dataNisjes;
+    private LocalDateTime dataArritjes;
+    private String aeroportiNisjes;
+    private String aeroportiArritjes;
+
+    public FlightsStatusDto(Long flightsId, String statusi, LocalDateTime dataNisjes, LocalDateTime dataArritjes, String aeroportiNisjes, String aeroportiArritjes) {
+        this.flightsId = flightsId;
+        this.statusi = statusi;
+        this.dataNisjes = dataNisjes;
+        this.dataArritjes = dataArritjes;
+        this.aeroportiNisjes = aeroportiNisjes;
+        this.aeroportiArritjes = aeroportiArritjes;
+    }
 }
