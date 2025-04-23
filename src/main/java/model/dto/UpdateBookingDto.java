4ed9be9e9994c20ecfa12274d2f966dfa5a2d6f9
GresaThaci <gresa.thaci@student.uni-pr.edu>
2025-04-23 22:46:05 +0200

    Update UpdateBookingDto.java

--- src/main/java/model/dto/UpdateBookingDto.java
+++ src/main/java/model/dto/UpdateBookingDto.java
@@ -1,4 +1,44 @@
 package model.dto;
 
-public class UpdateBookingDto {
-}
+
+    import java.time.LocalDateTime;
+
+    public class UpdateBookingDto {
+        private LocalDateTime dataENisjes;
+        private String destinacioni;
+        private Double cmimi;
+
+
+
+        public UpdateBookingDto(LocalDateTime dataENisjes, String destinacioni, Double cmimi) {
+            this.dataENisjes = dataENisjes;
+            this.destinacioni = destinacioni;
+            this.cmimi = cmimi;
+        }
+
+        public LocalDateTime getDataENisjes() {
+            return dataENisjes;
+        }
+
+        public void setDataENisjes(LocalDateTime dataENisjes) {
+            this.dataENisjes = dataENisjes;
+        }
+
+        public String getDestinacioni() {
+            return destinacioni;
+        }
+
+        public void setDestinacioni(String destinacioni) {
+            this.destinacioni = destinacioni;
+        }
+
+        public Double getCmimi() {
+            return cmimi;
+        }
+
+        public void setCmimi(Double cmimi) {
+            this.cmimi = cmimi;
+        }
+    }
+
+
