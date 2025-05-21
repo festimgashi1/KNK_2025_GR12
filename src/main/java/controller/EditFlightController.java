e6ada4ea6be70aa403a7ed673a70ae80db2497c6
Getoar19 <getoar.hoxha1@student.uni-pr.edu>
2025-05-21 19:59:17 +0200

    added language in this files EditFlight.fxml and EditFlightController.java

--- src/main/java/controller/EditFlightController.java
+++ src/main/java/controller/EditFlightController.java
@@ -1,12 +1,27 @@
 package controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import model.Flights;
 import services.FlightService;
+import services.LanguageManager;
+
+import java.util.ResourceBundle;
 
 public class EditFlightController {
+
+    @FXML private Label lblTitle;
+    @FXML private Label lblFlightNumber;
+    @FXML private Label lblPlaneId;
+    @FXML private Label lblDepartureAirport;
+    @FXML private Label lblArrivalAirport;
+    @FXML private Label lblDepartureTime;
+    @FXML private Label lblArrivalTime;
+    @FXML private Label lblDuration;
+    @FXML private Label lblStatus;
 
     @FXML private TextField txtFlightNumber;
     @FXML private TextField txtPlaneId;
@@ -16,6 +31,8 @@ public class EditFlightController {
     @FXML private TextField txtArrivalTime;
     @FXML private TextField txtDuration;
     @FXML private TextField txtStatus;
+
+    @FXML private Button btnSave;
 
     private final FlightService flightService = new FlightService();
     private Flights flightToEdit;
@@ -34,6 +51,29 @@ public class EditFlightController {
     }
 
     @FXML
+    public void initialize() {
+        loadTranslations();
+
+        // Register for language change updates
+        LanguageManager.getInstance().addListener(this::loadTranslations);
+    }
+
+    private void loadTranslations() {
+        ResourceBundle bundle = LanguageManager.getInstance().getResourceBundle();
+
+        lblTitle.setText(bundle.getString("edit.flight.title"));
+        lblFlightNumber.setText(bundle.getString("edit.flight.number"));
+        lblPlaneId.setText(bundle.getString("edit.plane.id"));
+        lblDepartureAirport.setText(bundle.getString("edit.departure.airport"));
+        lblArrivalAirport.setText(bundle.getString("edit.arrival.airport"));
+        lblDepartureTime.setText(bundle.getString("edit.departure.time"));
+        lblArrivalTime.setText(bundle.getString("edit.arrival.time"));
+        lblDuration.setText(bundle.getString("edit.duration"));
+        lblStatus.setText(bundle.getString("edit.status"));
+        btnSave.setText(bundle.getString("button.save.changes"));
+    }
+
+    @FXML
     public void handleUpdate() {
         flightToEdit.setPlaneid(Integer.parseInt(txtPlaneId.getText()));
         flightToEdit.setDepartureAirport(txtDepartureAirport.getText());
@@ -49,4 +89,3 @@ public class EditFlightController {
         }
     }
 }
-
